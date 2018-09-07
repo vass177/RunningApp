4b20f2b6ab3a6faadafdcb0005e4175abeac920c
Adam Vass <vass17@gmail.com>
2018-09-07 23:58:22 +0200

    Sign up layout further updates

--- app/src/main/java/com/triptech/run4fun/SignUpActivity.java
+++ app/src/main/java/com/triptech/run4fun/SignUpActivity.java
@@ -2,6 +2,11 @@ package com.triptech.run4fun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.triggertrap.seekarc.SeekArc;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +14,32 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        final TextView weightText=findViewById(R.id.weightText);
+        final TextView heightText=findViewById(R.id.heightText);
+        final SeekArc seekArcWeight=findViewById(R.id.seekWeight);
+        final SeekArc seekArcHeight=findViewById(R.id.seekHeight);
+        SeekArc.OnSeekArcChangeListener seekArcChangeListener= new SeekArc.OnSeekArcChangeListener() {
+            @Override
+            public void onProgressChanged(SeekArc seekArc, int progress, boolean b) {
+                if(seekArc.equals(seekArcWeight))
+                    weightText.setText(String.valueOf(progress)+" kg");
+                else
+                    heightText.setText(String.valueOf(progress)+" cm");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekArc seekArc) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekArc seekArc) {
+
+            }
+        };
+        seekArcWeight.setOnSeekArcChangeListener(seekArcChangeListener);
+        seekArcHeight.setOnSeekArcChangeListener(seekArcChangeListener);
     }
+
 }
